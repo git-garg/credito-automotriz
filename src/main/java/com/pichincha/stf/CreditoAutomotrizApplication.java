5c7e5b26b1eb864e0dfbe6cb6549d7bb134eaa69
unknown <dispositivo.gabrielreyes@gmail.com>
2022-07-01 17:23:14 -0500

    se sube informe

--- src/main/java/com/pichincha/stf/CreditoAutomotrizApplication.java
+++ src/main/java/com/pichincha/stf/CreditoAutomotrizApplication.java
@@ -1,17 +1,27 @@
 package com.pichincha.stf;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.opencsv.exceptions.CsvException;
 import com.pichincha.stf.service.ClienteServicio;
 import com.pichincha.stf.service.EjecutivoServicio;
 import com.pichincha.stf.service.MarcaServicio;
 import com.pichincha.stf.service.PatioServicio;
+import com.pichincha.stf.service.exception.ClienteException;
+import com.pichincha.stf.service.exception.CreditoAutomotrizException;
 
 @SpringBootApplication
 public class CreditoAutomotrizApplication implements CommandLineRunner {
+
+	private static final Logger log = LoggerFactory.getLogger(CreditoAutomotrizApplication.class);
 
 	private static final String RUTA = "static";
 	private static final String CLIENTES_CSV = "clientes.csv";
@@ -35,11 +45,15 @@ public class CreditoAutomotrizApplication implements CommandLineRunner {
 	}
 
 	@Override
-	public void run(String... args) throws Exception {
-		clienteServicio.cargarClientes(RUTA, CLIENTES_CSV);
-		patioServicio.cargarPatios();
-		ejecutivoServicio.cargarEjecutivos(RUTA, EJECUTIVOS_CSV);
-		marcaServicio.cargarMarcas(RUTA, MARCAS_CSV);
+	public void run(String... args) {
+		try {
+			clienteServicio.cargarClientes(RUTA, CLIENTES_CSV);
+			patioServicio.cargarPatios();
+			ejecutivoServicio.cargarEjecutivos(RUTA, EJECUTIVOS_CSV);
+			marcaServicio.cargarMarcas(RUTA, MARCAS_CSV);
+		} catch (IOException | URISyntaxException | CsvException | ClienteException | CreditoAutomotrizException e) {
+			log.error(e.getMessage());
+		}
 	}
 
 }
